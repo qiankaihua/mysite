eea3fa303b3a79b9e52b4b5f3ce6a0094c9d5720
qiankaihua <qiankaihua@126.com>
2017-08-08 00:06:06 +0800

    2017-8-8 00:06

--- resources/views/blog/list.blade.php
+++ resources/views/blog/list.blade.php
@@ -4,7 +4,7 @@
 @endsection
 @section('content')
     @foreach($blogs as $blog)
-    <div class="cd-full-width first-slide container row col-md-12 content first-content" style="margin-top: 40px">
+    <div class="cd-full-width container row col-md-12 content first-content" style="margin-top: 40px; text-align: center">
         <h1>{!! $blog->title !!}</h1>
         <p>{!! str_limit($blog->content, $limit = 10, $end = '...') !!}</p>
         <a href= {{ "/blog/".$blog->id }}>view more</a>
